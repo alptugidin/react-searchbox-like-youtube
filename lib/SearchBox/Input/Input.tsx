1f26572249674b4faf6ff3ea256ebbfcedd2e396
Alptuğ <alptugidin@gmail.com>
2022-11-19 05:35:44 +0300

    add mini modal for button hover

--- lib/SearchBox/Input/Input.tsx
+++ lib/SearchBox/Input/Input.tsx
@@ -16,10 +16,10 @@ const Input = (): JSX.Element => {
 
   const handleBoxBlur = (): void => {
     boxRef.current?.classList.remove('rounded-l-none');
-    leftSvgRef.current?.classList.add('md:hidden');
     boxRef.current?.classList.remove('border-blue-800');
     inputRef.current?.classList.remove('shadow-inner');
     leftSvgRef.current?.classList.remove('shadow-inner');
+    leftSvgRef.current?.classList.add('md:hidden');
   };
 
   return (
@@ -43,8 +43,11 @@ const Input = (): JSX.Element => {
       </div>
       <button
         type='button'
-        className='w-16 h-10 flex justify-center items-center bg-gray-100 border border-l-0 border-gray-300 rounded-r-full hover:bg-gray-200 transition-all'>
+        className='group w-16 h-10 flex justify-center items-center bg-gray-100 border border-l-0 border-gray-300 rounded-r-full hover:bg-gray-200 transition-all relative'>
         <Search size='normal'/>
+        <div className='absolute top-14 bg-gray-500 text-white text-xs px-2 py-2 rounded-[4px] hidden group-hover:block'>
+          <p>Search</p>
+        </div>
       </button>
     </div>
   );
