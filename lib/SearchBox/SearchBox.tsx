e00678d808dfb50d014edcb61c600406ed2a64bb
Alptuğ <alptugidin@gmail.com>
2022-11-20 03:37:41 +0300

    add context api

--- lib/SearchBox/SearchBox.tsx
+++ lib/SearchBox/SearchBox.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { createContext, useEffect, useRef, useState } from 'react';
 import { Input } from './Input';
 import { Results } from './Results';
+import useIsMobile from './hooks/useIsMobile';
+import { ISearchBoxContext } from '../types';
 
-const SearchBox = (): JSX.Element => {
+export const SearchBoxContext = createContext<ISearchBoxContext>({} as ISearchBoxContext);
+const SearchBox: React.FC = () => {
+  const { isMobile } = useIsMobile();
+  const [showSB, setShowSB] = useState(false);
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const value = {
+    isMobile,
+    mainRef,
+    setShowSB,
+    showSB
+  };
+
+  useEffect(() => {
+    const listener = (e: MouseEvent): void => {
+      if (mainRef.current != null && !mainRef.current?.contains(e.target as Node) && isMobile) {
+        mainRef.current?.classList.remove('!absolute', 'left-0', 'right-0', 'w-full', 'top-0', 'bg-white');
+        setShowSB(false);
+      }
+    };
+    window.addEventListener('click', listener);
+    return () => window.removeEventListener('click', listener);
+  }, [isMobile]);
+
   return (
-    <div>
-      <Input/>
-      <Results/>
+    <div
+      ref={mainRef}
+      className='relative w-full z-30'>
+      <SearchBoxContext.Provider value={value}>
+        <Input/>
+        <Results/>
+      </SearchBoxContext.Provider>
     </div>
   );
 };
