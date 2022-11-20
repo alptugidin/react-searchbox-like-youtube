61c8d766f2b547d92a0c123a63d73eb315f04ba7
Alptuğ <alptugidin@gmail.com>
2022-11-21 01:17:34 +0300

    add new props in Context interface

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -2,6 +2,17 @@ import React from 'react';
 export interface ISearchBoxContext {
     isMobile: boolean
     mainRef: React.RefObject<HTMLDivElement>
+    topRef: React.RefObject<HTMLDivElement>
+    rightDivRef: React.RefObject<HTMLDivElement>
+    middleDivRef: React.RefObject<HTMLDivElement>
+    searchButtonRef: React.RefObject<HMLTButtonElement>
+    backButtonRef: React.RefObject<HMLTButtonElement>
+    inputRef: React.RefObject<HTMLInputElement>
     setShowSB: React.Dispatch<React.SetStateAction<boolean>>
     showSB: boolean
+    setBlurSB: () => void
+}
+
+export interface ISvg {
+    size?: string
 }
