88240cc0a6c12ce2cc6f522e35c560491e25c7b6
Alptuğ <alptugidin@gmail.com>
2022-11-23 03:47:11 +0300

    add new tests for Results component

---
+++ __tests__/Results.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import jest from 'jest-mock';
+import '@testing-library/jest-dom';
+import { findByRole, fireEvent, queryByRole, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SearchBox from '../lib';
+import { ISearchResult } from '../lib/types';
+
+const results = [
+  { id: 1, title: 'Javascript tutorials' },
+  { id: 2, title: 'Java tutorials' },
+  { id: 3, title: 'Kotlin tutorials' },
+  { id: 4, title: 'Swift tutorials' },
+  { id: 5, title: 'Dart tutorials' },
+  { id: 6, title: 'Python tutorials' },
+  { id: 7, title: 'Rust tutorials' }
+];
+
+const searchTerms = ['javascript', 'kotlin', 'rust', 'avascr', 'otl'];
+const randomTerm = searchTerms[Math.floor(Math.random() * searchTerms.length)];
+
+beforeEach(() => render(<SearchBox results={results} onChange={mockOnChange} />));
+
+const mockOnChange = jest.fn();
+it('should matched that the search term and the results', async () => {
+  const input = screen.getByPlaceholderText('Search something');
+  await userEvent.type(input, randomTerm);
+  const ul = screen.getByRole('listResults');
+  const regexpToHave = new RegExp(`(${randomTerm})`, 'gi');
+  expect(ul).toHaveTextContent(regexpToHave);
+});
