b0ec677f3ddc09a0d252c4bb1bdfcd123d5d3488
Alptuğ <alptugidin@gmail.com>
2022-11-25 05:31:44 +0300

    new ClearSVG

---
+++ lib/SearchBox/Svg/Clear.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ClearSVG: React.FC = () => {
+  return (
+    <div>
+      <svg
+        viewBox="0 0 24 24"
+        focusable="false">
+        <g>
+          <path d="M12.7,12l6.6,6.6l-0.7,0.7L12,12.7l-6.6,6.6l-0.7-0.7l6.6-6.6L4.6,5.4l0.7-0.7l6.6,6.6l6.6-6.6l0.7,0.7L12.7,12z" >
+          </path>
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default ClearSVG;
