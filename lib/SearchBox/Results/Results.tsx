cf11e41d6fa5b84cbf340977e13ddb09e5d9ea1d
Alptuğ <alptugidin@gmail.com>
2022-11-26 05:28:31 +0300

    fix conditional classes

--- lib/SearchBox/Results/Results.tsx
+++ lib/SearchBox/Results/Results.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { ISearchResult } from '../../types';
 import { useSearchBoxContext } from '../SearchBox';
 import { Search } from '../Svg';
+import Arrow from '../Svg/Arrow';
 
 const Results = (): JSX.Element => {
   const cx = useSearchBoxContext();
@@ -9,17 +10,17 @@ const Results = (): JSX.Element => {
 
   const filteringCondition = (title: string): string | undefined => {
     if (cx.value === '') return undefined;
-    if (title.toLowerCase().includes(cx.value.toLowerCase())) {
+    if (cx.value.length > 1 && title.toLowerCase().includes(cx.value.toLowerCase())) {
       return title;
     }
   };
 
   const highlightedResult = (title: string): JSX.Element => {
-    let span = <span >{title}</span>;
+    let span = <span className=''>{title}</span>;
     const splitted = title.split(new RegExp(`(${cx.value})`, 'gi'));
     if (splitted.length > 1) {
       span =
-        <div className={'sb-highlight-div'}>
+        <div className='text-sm md:text-base'>
           <span >{splitted[0]}</span>
           <span className='font-semibold' >{splitted[1]}</span>
           <span >{splitted[2]}</span>
@@ -33,25 +34,40 @@ const Results = (): JSX.Element => {
     cx.setValue('');
   };
 
+  const handleSelect = (title: string): void => {
+    cx.setValue(title);
+    cx.setTempVal(title);
+    cx.inputRef.current?.focus();
+  };
+
   useEffect(() => {
     setResultsLen(cx.results.filter((item) => filteringCondition(item.title)).length);
   }, [cx.value]);
 
   return (
-    <div className='w-[calc(100%_-_33px)] bg-white custom-box-shadow rounded-xl absolute top-11 -left-[26px] overflow-hidden'>
+    <div className='md:w-[calc(100%_-_33px)] w-full bg-white custom-box-shadow md:rounded-xl rounded-none absolute md:top-11 top-[48px] md:-left-[26px] left-0 md:overflow-hidden overflow-visible'>
       {cx.value !== '' && resultsLen > 0 && (
-        <ul role='listResults' className='py-3'>
+        <ul role='listResults' className='md:py-3'>
+          <div className='bg-white absolute left-0 -top-1 right-0 h-1.5'></div>
           {
             cx.results.filter(item => filteringCondition(item.title)).map(item => (
-              <li key={item.id} className=''>
+              <li key={item.id} className='flex h-[38px] items-center border-b-[1px] md:border-none border-gray-100 md:py-0 md:px-0 pl-2'>
                 <button
                   type='button'
                   onClick={() => handleOnClick(item)}
-                  className='flex items-center cursor-default hover:bg-gray-100 h-8 leading-8 w-full'>
-                  <div className='w-12 flex justify-center items-center'>
-                    <Search size='mini' />
-                  </div>
+                  className='flex items-center cursor-default md:hover:bg-gray-100 h-8 leading-8 w-full'>
+                  { !cx.isMobile &&
+                    <div className='w-12 flex justify-center items-center'>
+                      <Search size='mini' />
+                    </div>
+                  }
                   {highlightedResult(item.title)}
+                </button>
+                <button
+                  type='button'
+                  onClick={() => handleSelect(item.title)}
+                  className='bg-gray-100 w-[38px] h-full flex items-center justify-center'>
+                  <Arrow/>
                 </button>
               </li>
             ))
