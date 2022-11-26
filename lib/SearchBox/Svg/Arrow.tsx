bc3424121a7c6f1a55b136c5019afe08e9ccd959
Alptuğ <alptugidin@gmail.com>
2022-11-26 05:28:35 +0300

    first

---
+++ lib/SearchBox/Svg/Arrow.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Arrow: React.FC = () => {
+  return (
+    <div>
+      <svg width="13" height="13" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="22" height="6" fill="#B7B7B7"/>
+        <rect width="22" height="6" transform="translate(0 22) rotate(-90)" fill="#B7B7B7"/>
+        <rect width="32.8325" height="6" transform="translate(23.2158 27.4585) rotate(-135)" fill="#B7B7B7"/>
+      </svg>
+    </div>
+  );
+};
+
+export default Arrow;
