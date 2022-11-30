80f9d68aaf28d1cf4acb39d006efbd962064f63f
Alptuğ <alptugidin@gmail.com>
2022-11-30 05:14:48 +0300

    new component for night mode

---
+++ src/components/NightModeButton.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useSearchBoxContext } from '../../lib/SearchBox/SearchBox';
+
+interface INightModeButton {
+    setNightMode: (value: React.SetStateAction<boolean>) => void
+    nightMode: boolean
+
+}
+
+const NightModeButton: React.FC<INightModeButton> = (props) => {
+  const { setNightMode, nightMode } = props;
+  //   useEffect(() => {
+  //     if (nightMode) {
+  //       document.body.style.background = 'black';
+  //     } else {
+  //       document.body.style.background = 'white';
+  //     }
+  //   }, [nightMode]);
+  return (
+    <button
+      type='button'
+      onClick={() => setNightMode(!nightMode)}
+      className='relative w-[40px] h-[40px] rounded-full bg-red-600 overflow-hidden'>
+      <img
+        src="./sun.svg"
+        alt="sun"
+        className={`absolute right-0 top left-0 ml-auto mr-auto transition-all ${nightMode ? '-bottom-[38px]' : 'bottom-[8px]'}`}
+      />
+      <img
+        src="./moon.svg"
+        alt="moon"
+        className={`absolute right-0 left-0 ml-auto mr-auto transition-all ${nightMode ? 'bottom-[8px]' : '-bottom-[38px]'}`}
+      />
+    </button>
+  );
+};
+
+export default NightModeButton;
