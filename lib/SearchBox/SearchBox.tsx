75990491b2a5433cc5d5be878ab4e4219b239f94
Alptuğ <alptugidin@gmail.com>
2022-11-30 05:13:24 +0300

    remove unused functions

--- lib/SearchBox/SearchBox.tsx
+++ lib/SearchBox/SearchBox.tsx
@@ -4,10 +4,18 @@ import { Results } from './Results';
 import { ISearchBoxContext, ISearchBoxProps, ISearchResult } from '../types';
 import useIsMobile from '../hooks/useIsMobile';
 import { Clear, Search } from './Svg';
+import DummyInput from './DummyInput/DummyInput';
 
 const SearchBoxContext = createContext<ISearchBoxContext>({} as ISearchBoxContext);
 
-const SearchBox: React.FC<ISearchBoxProps> = ({ onChange, onClick, results }) => {
+const SearchBox: React.FC<ISearchBoxProps> = (
+  {
+    onChange,
+    onClick,
+    results,
+    sx = { backgroundColor: '#ffffff' }
+  }
+) => {
   const mainRef = useRef<HTMLDivElement>(null);
   const topRef = useRef<HTMLDivElement>(null);
   const searchButtonRef = useRef<HTMLButtonElement>(null);
@@ -20,49 +28,23 @@ const SearchBox: React.FC<ISearchBoxProps> = ({ onChange, onClick, results }) =>
   const leftSvgRef = useRef<HTMLDivElement>(null);
   const modalRef = useRef<HTMLDivElement>(null);
   /** @brancRef */
+  const [showLeftSearchSvg, setShowLeftSearchSvg] = useState(false);
+  const [showDummyInput, setShowDummyInput] = useState(false);
   const respBgRef = useRef<HTMLDivElement>(null);
   const inputSearchIconRef = useRef<HTMLDivElement>(null);
   const clearButtonRef = useRef<HTMLButtonElement>(null);
-  const mockInputRef = useRef<HTMLDivElement>(null);
+  const dummyInputRef = useRef<HTMLDivElement>(null);
   const respSbButton = useRef<HTMLButtonElement>(null);
   /** @brancRef */
 
   const { isMobile } = useIsMobile();
-  const [showSB, setShowSB] = useState(false);
+  const [showSB, setShowSB] = useState(true);
   const [value, setValue] = useState('');
   const [tempVal, setTempVal] = useState('');
   const [filteredResults, setFilteredResults] = useState<ISearchResult[]>([]);
 
-  const setBlurSB = (): void => {
-    if (isMobile) {
-      setShowSB(false);
-      // mainRef.current?.classList.remove('!absolute', 'left-0', 'right-0', 'w-full', 'top-0', 'bg-white');
-      topRef.current?.classList.remove('custom-box-shadow');
-      topRef.current?.classList.add('justify-end');
-      searchButtonRef.current?.classList.remove('bg-gray-100', 'rounded-r-full');
-      backButtonRef.current?.classList.add('hidden');
-      backButtonRef.current?.classList.remove('flex');
-      rightDivRef.current?.classList.add('hidden');
-      middleDivRef.current?.classList.remove('basis-9/12');
-    } else {
-      leftSvgRef.current?.classList.add('md:hidden');
-      setValue('');
-      if (!isMobile) {
-        boxRef.current?.classList.remove('rounded-l-none', '!border-blue-800');
-        inputRef.current?.classList.remove('shadow-inner');
-        leftSvgRef.current?.classList.remove('shadow-inner');
-      }
-    }
-  };
-
   const handleClick = (): void => {
-    mainRef.current?.classList.remove('!hidden');
-    inputRef.current?.focus();
-  };
-
-  const handleMockInput = (): void => {
-    mockInputRef.current?.classList.add('hidden');
-    handleClick();
+    setShowSB(true);
   };
 
   const ctxValue = {
@@ -80,7 +62,6 @@ const SearchBox: React.FC<ISearchBoxProps> = ({ onChange, onClick, results }) =>
     modalRef,
     showSB,
     setShowSB,
-    setBlurSB,
     results,
     onChange,
     onClick,
@@ -94,41 +75,49 @@ const SearchBox: React.FC<ISearchBoxProps> = ({ onChange, onClick, results }) =>
     inputSearchIconRef,
     clearButtonRef,
     respBgRef,
-    mockInputRef,
-    respSbButton
+    dummyInputRef,
+    respSbButton,
+    showLeftSearchSvg,
+    setShowLeftSearchSvg,
+    showDummyInput,
+    setShowDummyInput
     /** @branchRef */
   };
 
   useEffect(() => {
     const listener = (e: MouseEvent): void => {
       if (!mainRef.current?.contains(e.target as HTMLDivElement)) {
-        inputSearchIconRef.current?.classList.add('!hidden');
+        setShowLeftSearchSvg(false);
         setValue('');
       }
-      if (respBgRef.current?.contains(e.target as HTMLDivElement)) {
-        mainRef.current?.classList.add('!hidden');
-        respSbButton.current?.classList.remove('hidden');
+      if (isMobile) {
+        if (respBgRef.current?.contains(e.target as HTMLDivElement)) {
+          setShowSB(false);
+          setShowDummyInput(false);
+        }
       }
     };
     window.addEventListener('click', listener);
     return () => window.removeEventListener('click', listener);
   }, []);
 
+  useEffect(() => {
+    if (isMobile) {
+      setShowSB(false);
+    } else {
+      setShowSB(true);
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile && showSB) inputRef.current?.focus();
+  }, [showSB]);
+
   return (
     <SearchBoxContext.Provider value={ctxValue}>
       { isMobile &&
           <div className='flex'>
-            <div
-              ref={mockInputRef}
-              onClick={handleMockInput}
-              className='w-full hidden'>
-              <div className='w-full h-8 bg-gray-100 rounded-lg items-center px-1.5 flex justify-between'>
-                <span className='truncate text-sm text-gray-500'>{tempVal}</span>
-                <div className='w-6 h-6'>
-                  <Clear/>
-                </div>
-              </div>
-            </div>
+            {showDummyInput && <DummyInput/> }
             <button
               ref={respSbButton}
               role='responsive-search-button'
@@ -139,13 +128,15 @@ const SearchBox: React.FC<ISearchBoxProps> = ({ onChange, onClick, results }) =>
             </button>
           </div>
       }
+      {showSB &&
       <div
         ref={mainRef}
         id='sbly'
-        className={`searchbox ${isMobile ? '!hidden' : ''}`}>
+        className='searchbox'>
         <Input/>
         <Results/>
       </div>
+      }
     </SearchBoxContext.Provider>
   );
 };
