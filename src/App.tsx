741567e2d979a5b1fa4bc0503a184a6b09406ab6
Alptuğ <alptugidin@gmail.com>
2022-12-01 05:16:49 +0300

    add dynamic background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import NightModeButton from './components/NightModeButton';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBox from '../lib';
 import { ISearchResult } from '../lib/types';
 const App: React.FC = () => {
   const [nightMode, setNightMode] = useState(false);
+  const [bg, setBg] = useState('#ffffff');
   const [results] = useState(
     [
       { id: 0, title: 'Javascript tutorials' },
@@ -26,20 +27,31 @@ const App: React.FC = () => {
   const handleOnClick = (onClickData: ISearchResult): void => {
   };
 
+  useEffect(() => {
+    setBg(nightMode ? 'black' : 'white');
+  }, [nightMode]);
+
   return (
-    <div className='h-12 w-full  flex justify-evenly items-center'>
-      <div className='md:w-3/12 w-2/12 flex justify-start'>
-        <div className='md:w-1/2 w-full h-10 rounded-full bg-gradient-to-r from-red-700 to-red-400'></div>
-      </div>
-      <div className='md:w-[500px] w-6/12'>
-        <SearchBox
-          onChange={handleOnChange}
-          onClick={handleOnClick}
-          results={results}
-        />
-      </div>
-      <div className='md:w-3/12 w-2/12 flex justify-end'>
-        <NightModeButton {...{ setNightMode, nightMode }}/>
+    <div className={`h-screen transition-all ${nightMode ? 'bg-[#0F0F0F]' : 'bg-white'}`}>
+      <div className='w-full  flex justify-evenly h-12 items-center'>
+        <div className='md:w-3/12 w-2/12 flex justify-start'>
+          <div className='md:w-1/2 w-full h-10 rounded-full bg-gradient-to-r from-red-700 to-red-500'></div>
+        </div>
+        <div className='md:w-[500px] w-6/12'>
+          <SearchBox
+            onChange={handleOnChange}
+            onClick={handleOnClick}
+            results={results}
+            nightMode={nightMode}
+            // sx={{
+            //   lightBg: '#FFFFFF', // nightMode off
+            //   darkBg: '#0F0F0F' // nightMode on
+            // }}
+          />
+        </div>
+        <div className='md:w-3/12 w-2/12 flex justify-end'>
+          <NightModeButton {...{ setNightMode, nightMode }}/>
+        </div>
       </div>
     </div>
   );
