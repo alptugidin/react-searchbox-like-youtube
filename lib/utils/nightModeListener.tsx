6cd837d0f78816d5b86977d6d45697a4e0a1b1e7
Alptuğ <alptugidin@gmail.com>
2022-12-02 05:25:44 +0300

    new component to listen nightMode prop

---
+++ lib/utils/nightModeListener.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useSearchBoxContext } from '../SearchBox/SearchBox';
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+const nightModeListener = (nightMode: boolean): void => {
+  useEffect(() => {
+    if (nightMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [nightMode]);
+};
+
+export default nightModeListener;
