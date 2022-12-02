94a55327402caeda2512792dc36a07ad7b54dc9d
Alptuğ <alptugidin@gmail.com>
2022-12-02 05:25:53 +0300

    add dark class

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from 'react';
 import SearchBox from '../lib';
 import { ISearchResult } from '../lib/types';
 const App: React.FC = () => {
-  const [nightMode, setNightMode] = useState(false);
-  const [bg, setBg] = useState('#ffffff');
+  const [nightMode, setNightMode] = useState(true);
   const [results] = useState(
     [
       { id: 0, title: 'Javascript tutorials' },
@@ -27,12 +26,8 @@ const App: React.FC = () => {
   const handleOnClick = (onClickData: ISearchResult): void => {
   };
 
-  useEffect(() => {
-    setBg(nightMode ? 'black' : 'white');
-  }, [nightMode]);
-
   return (
-    <div className={`h-screen transition-all ${nightMode ? 'bg-[#0F0F0F]' : 'bg-white'}`}>
+    <div className={'h-screen transition-all bg-white dark:bg-[#0f0f0f]'}>
       <div className='w-full  flex justify-evenly h-12 items-center'>
         <div className='md:w-3/12 w-2/12 flex justify-start'>
           <div className='md:w-1/2 w-full h-10 rounded-full bg-gradient-to-r from-red-700 to-red-500'></div>
