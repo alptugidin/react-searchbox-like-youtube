343a55f6a6a8bd86790902ba8e6e1cefdfbacc2c
Alptuğ <alptugidin@gmail.com>
2022-12-02 05:24:39 +0300

    add conditional fix

--- lib/SearchBox/Svg/Back.tsx
+++ lib/SearchBox/Svg/Back.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Back: React.FC = () => {
   return (
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><path d="M21,11v1H5.64l6.72,6.72l-0.71,0.71L3.72,11.5l7.92-7.92l0.71,0.71L5.64,11H21z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg"
+        enableBackground="new 0 0 24 24"
+        height="24"
+        className='fill-black dark:fill-white'
+        viewBox="0 0 24 24"
+        width="24"
+      >
+        <path d="M21,11v1H5.64l6.72,6.72l-0.71,0.71L3.72,11.5l7.92-7.92l0.71,0.71L5.64,11H21z"></path></svg>
     </div>
   );
 };
