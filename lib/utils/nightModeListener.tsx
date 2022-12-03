0d3da36b46ba038750a5967147fa6726e2a231f0
Alptuğ <alptugidin@gmail.com>
2022-12-03 05:14:28 +0300

    fix fouc

--- lib/utils/nightModeListener.tsx
+++ lib/utils/nightModeListener.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSearchBoxContext } from '../SearchBox/SearchBox';
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
-}
+// if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//   document.documentElement.classList.add('dark');
+// } else {
+//   document.documentElement.classList.remove('dark');
+// }
 const nightModeListener = (nightMode: boolean): void => {
   useEffect(() => {
     if (nightMode) {
