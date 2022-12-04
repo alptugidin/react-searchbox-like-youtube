2784385a9f0907dd21957681082ee8f8d08c5dbd
Alptuğ <alptugidin@gmail.com>
2022-12-05 01:38:19 +0300

    fix ref

--- lib/SearchBox/DummyInput/DummyInput.tsx
+++ lib/SearchBox/DummyInput/DummyInput.tsx
@@ -5,15 +5,16 @@ import { Clear } from '../Svg';
 const DummyInput: React.FC = () => {
   const ctx = useSearchBoxContext();
 
-  const handleMockInput = (): void => {
+  const handleDummyInput = (): void => {
     ctx.setShowSB(true);
-    ctx.inputRef.current?.focus();
+    ctx.refs.input.current?.focus();
   };
 
   return (
     <div
-      ref={ctx.dummyInputRef}
-      onClick={handleMockInput}
+      ref={ctx.refs.dummyInput}
+      onClick={handleDummyInput}
+      role='dummy-input'
       className='w-full'>
       <div className='w-full h-8 bg-gray-100 dark:dark-bg-secondary rounded-lg items-center px-1.5 flex justify-between'>
         <span className='truncate text-sm text-gray-500 dark:text-white'>{ctx.tempVal}</span>
