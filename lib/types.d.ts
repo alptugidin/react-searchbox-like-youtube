db7ee6d3c2a69fb2c24ac277b32e66b523f42657
Alptuğ <alptugidin@gmail.com>
2022-12-04 04:55:01 +0300

    fix refs

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -9,9 +9,10 @@ export interface ISearchResult {
     title: string
 }
 export interface ISearchBoxProps {
-    results: ISearchResult[]
+    results: ISearchResult[] | undefined
     onChange: (onChangeData: string) => void
     onClick: (onClickData: ISearchResult) => void
+    onSearch: (onSearchData: string | ISearchResult) => void
     nightMode?: boolean
     sx?: {
         lightBg?: string
@@ -20,20 +21,19 @@ export interface ISearchBoxProps {
 }
 
 export interface ISearchBoxContext extends Pick<ISearchBoxProps,
-    'results' | 'onChange' | 'onClick' | 'sx' | 'nightMode'
+    'results' | 'onChange' | 'onClick' | 'sx' | 'nightMode' | 'onSearch'
     > {
+    refs: {
+        main: React.RefObject<HTMLDivElement>
+        input: React.RefObject<HTMLInputElement>
+        result: React.RefObject<HTMLDivElement>
+        respBg: React.RefObject<HTMLDivElement>
+        inputSearchIcon: React.RefObject<HTMLDivElement>
+        clearButton: React.RefObject<HTMLButtonElement>
+        dummyInput: React.RefObject<HTMLDivElement>
+        respSbButton: React.RefObject<HTMLButtonElement>
+    }
     isMobile: boolean
-    mainRef: React.RefObject<HTMLDivElement>
-    topRef: React.RefObject<HTMLDivElement>
-    rightDivRef: React.RefObject<HTMLDivElement>
-    middleDivRef: React.RefObject<HTMLDivElement>
-    searchButtonRef: React.RefObject<HTMLButtonElement>
-    backButtonRef: React.RefObject<HTMLButtonElement>
-    inputRef: React.RefObject<HTMLInputElement>
-    resultRef: React.RefObject<HTMLDivElement>
-    boxRef: React.RefObject<HTMLDivElement>
-    leftSvgRef: React.RefObject<HTMLDivElement>
-    modalRef: React.RefObject<HTMLDivElement>
     setShowSB: React.Dispatch<React.SetStateAction<boolean>>
     showSB: boolean
     filteredResults: ISearchResult[]
@@ -42,19 +42,16 @@ export interface ISearchBoxContext extends Pick<ISearchBoxProps,
     setValue: React.Dispatch<React.SetStateAction<string>>
     tempVal: string
     setTempVal: React.Dispatch<React.SetStateAction<string>>
-    /** @branchType */
-    inputSearchIconRef: React.RefObject<HTMLDivElement>
-    clearButtonRef: React.RefObject<HTMLButtonElement>
-    respBgRef: React.RefObject<HTMLDivElement>
-    dummyInputRef: React.RefObject<HTMLDivElement>
-    respSbButton: React.RefObject<HTMLButtonElement>
     showLeftSearchSvg: boolean
     setShowLeftSearchSvg: React.Dispatch<React.SetStateAction<boolean>>
     showDummyInput: boolean
     setShowDummyInput: React.Dispatch<React.SetStateAction<boolean>>
     lightBg: string
     darkBg: string
-    /** @branchType */
+    arr: ISearchResult[] | undefined
+    setArr: React.Dispatch<React.SetStateAction<ISearchResult[] | undefined>>
+    active: number
+    setActive: React.Dispatch<React.SetStateAction<number>>
 }
 
 export interface ISearchBoxProvider {
