a180ebb3108f9909997db73fe3a4fbed1e807f31
Alptuğ <alptugidin@gmail.com>
2022-12-04 04:56:50 +0300

    change api url

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -9,24 +9,24 @@ export const useFetch = (param: string): IFetchMovies => {
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    fetch(`https://www.omdbapi.com/?apikey=9d6d5955&s=${param}`)
-      .then(async res => await res.json())
-      .then(result => {
-        setResults(result.Search);
-      })
-      .catch(err => setError(err));
+    if (param !== undefined && param.length > 1) {
+      const arr: ISearchResult[] = [];
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=204f2cebe811d76c47a873f7233cf17a&language=en-US&query=${param}&page=1&include_adult=false`)
+        .then(async res => await res.json())
+        .then(response => {
+          response.results.forEach((data: any) => {
+            const el: ISearchResult = {
+              id: data.id,
+              title: data.title
+            };
+            arr.push(el);
+          });
+        })
+        .catch(err => setError(err))
+        .finally(() => {
+          setResults(arr);
+        });
+    }
   }, [param]);
   return { asyncResults };
 };
-
-/**
- *    "Search": [
-        {
-            "Title": "The Tit and the Moon",
-            "Year": "1994",
-            "imdbID": "tt0111403",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BZjcwNTE2YzgtNjM0OC00MzA3LTg3ZjctYzkwYmJhNTBhY2FmXkEyXkFqcGdeQXVyMTA0MjU0Ng@@._V1_SX300.jpg"
-        },
- *
- */
