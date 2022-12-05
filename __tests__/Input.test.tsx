6d05cfbb95a7bd93994341d0e51c01bcece3a725
Alptuğ <alptugidin@gmail.com>
2022-12-05 05:30:41 +0300

    add unit tests

--- __tests__/Input.test.tsx
+++ __tests__/Input.test.tsx
@@ -1,11 +1,76 @@
 import React, { createContext, useDeferredValue } from 'react';
 import jest from 'jest-mock';
-import { queryByPlaceholderText, queryByRole, render, screen } from '@testing-library/react';
+import { getByRole, queryByPlaceholderText, queryByRole, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SearchBox from '../lib';
-import { ISearchBoxContext, ISearchResult } from '../lib/types';
-import { Input } from '../lib/SearchBox/Input';
 
-test('handleClear', () => {
-  const SearchBoxContext = createContext<ISearchBoxContext>({} as ISearchBoxContext);
+const results = [
+  { id: 0, title: 'Javascript tutorials' },
+  { id: 1, title: 'Javascript tutorials 2' },
+  { id: 2, title: 'Java tutorials' },
+  { id: 3, title: 'Kotlin tutorials' },
+  { id: 4, title: 'Swift tutorials' },
+  { id: 5, title: 'Dart tutorials' },
+  { id: 6, title: 'Python tutorials' },
+  { id: 7, title: 'Rust tutorials' },
+  { id: 8, title: 'C# tutorials' },
+  { id: 9, title: 'Perl tutorials' },
+  { id: 10, title: 'C++ tutorials' }
+];
+
+const mockFn = jest.fn();
+test('handleClear', async () => {
+  render(
+    <SearchBox
+      results={[]}
+      onChange={mockFn}
+      onClick={mockFn}
+      onSearch={mockFn}
+    />);
+  const input = screen.getByPlaceholderText('Search something');
+  await userEvent.type(input, 'javascript');
+  expect(input).toHaveValue('javascript');
+  const clear = screen.getByRole('clear');
+  await userEvent.click(clear);
+  expect(input).toHaveValue('');
+});
+
+test('handleSearch', async () => {
+  const { container } = render(
+    <SearchBox
+      results={[]}
+      onChange={mockFn}
+      onClick={mockFn}
+      onSearch={mockFn}
+    />);
+  const input = screen.getByPlaceholderText('Search something');
+  const searchButton = screen.getByRole('search-button');
+  await userEvent.type(input, 'foo');
+  expect(queryByRole(container, 'inputSearchIcon')).toBeVisible();
+  await userEvent.click(searchButton);
+  expect(queryByRole(container, 'inputSearchIcon')).toEqual(null);
+  expect(input).toHaveValue('foo');
+});
+
+test('handleKeyDown', async () => {
+  const { container } = render(
+    <SearchBox
+      results={results}
+      onChange={mockFn}
+      onClick={mockFn}
+      onSearch={mockFn}
+    />);
+  const input = screen.getByPlaceholderText('Search something');
+  await userEvent.type(input, 'tut');
+  // case ArrowDown (twice)
+  await userEvent.keyboard('{arrowdown}{arrowdown}');
+  const ul = screen.getByRole('search-results');
+  expect(ul.childNodes[1]).toHaveClass('bg-[#00000010]');
+  // case ArrowUp (once)
+  await userEvent.keyboard('{arrowup}');
+  expect(ul.childNodes[1]).not.toHaveClass('bg-[#00000010]');
+  expect(ul.childNodes[0]).toHaveClass('bg-[#00000010]');
+  // case BackSpace
+  await userEvent.type(input, '{backspace>30}');
+  expect(input).toHaveValue('');
 });
